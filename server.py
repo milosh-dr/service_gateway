f320b900343dce762c3f9bcc53b2d4f222841e27
milosh-dr <milosh-dr@wp.pl>
2023-10-11 20:17:12 +0000

    Initial commit

---
+++ server.py
@@ -0,0 +1,8 @@
+import os, gridfs, pika, json
+from flask import Flask, request
+from flask_pymongo import PyMongo
+from auth import validate
+from auth_svc import access
+from storage import util
+
+server == Flask(__name__)
